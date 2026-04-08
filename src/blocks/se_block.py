ab87eef9249109d90bea32fac63a79fa6e6ebbd0
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-04-08 08:58:46 +0300

    Create se_block.py

---
+++ src/blocks/se_block.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+class SEBlock(nn.Module):
+    def __init__(self, channels, reduction=16):
+        super(SEBlock, self).__init__()
+        self.fc = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(channels, channels // reduction, kernel_size=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channels // reduction, channels, kernel_size=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        scale = self.fc(x)
+        return x * scale
