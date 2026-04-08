ff843de135fee820b67ac518452b9d216e1c54c9
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-04-08 09:03:16 +0300

    Create decoder2.py

---
+++ src/decoder/decoder/decoder2.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+from ..blocks.upsample import UpSample
+from ..blocks.conv_block import ConvBlock
+from ..blocks.se_block import SEBlock
+
+class Decoder2(nn.Module):
+    def __init__(self, features=[512,256,128,64]):
+        super(Decoder2, self).__init__()
+        self.up = nn.ModuleList()
+        self.conv = nn.ModuleList()
+        self.se = nn.ModuleList()
+
+        for f in features:
+            self.up.append(UpSample(scale_factor=2))
+            self.conv.append(ConvBlock(f*3, f))  
+            self.se.append(SEBlock(f))
+
+    def forward(self, x, skips1, skips2):
+        for i in range(len(self.up)):
+            x = self.up[i](x)
+            x = torch.cat([x, skips1[-(i+1)], skips2[-(i+1)]], dim=1)
+            x = self.conv[i](x)
+            x = self.se[i](x)
+        return x
